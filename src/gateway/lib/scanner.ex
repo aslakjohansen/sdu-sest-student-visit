959cc34056373237e88d59a3e091b5e2950ad4d1
Aslak Johansen <aslakj@gmail.com>
2021-10-22 12:09:53 +0200

    Cleanup

--- src/gateway/lib/scanner.ex
+++ src/gateway/lib/scanner.ex
@@ -16,51 +16,6 @@ defmodule Scanner do
     IO.puts("Scanning ...")
     
     uarts = Circuits.UART.enumerate()
-#    
-#    previous = Map.keys(state) |> MapSet.new()
-#    current  = Map.keys(uarts) |> MapSet.new()
-#    
-#    appearances    = MapSet.difference(current, previous) |> MapSet.to_list()
-#    disappearances = MapSet.difference(previous, current) |> MapSet.to_list()
-#    
-#    # removed devices
-#    state = disappearances
-#          |>Enum.reduce(state, fn device, state -> Map.delete(state, device) end)
-##    for device <- disappearances do
-##      # supervisor
-##      Map.delete(state, device)
-##    end
-#    
-#    # new devices
-#    adder = fn device, state ->
-#      description = uarts[device]
-#      {match, speed} =
-#      case description do
-#        %{description:  "SmartEverything Fox3",
-#          manufacturer: "Arduino LLC"}
-#          -> {true, 9600}
-#        %{description:  "CP2102 USB to UART Bridge Controller",
-#          manufacturer: "Silicon Labs"}
-#          -> {true, 115200}
-#        _ -> {false, nil}
-#      end
-#      if match do
-#        topic = "dk/sdu/sest/test"
-#        case Gateway.start_link(topic, device, speed) do
-#          {:ok, pid} ->
-#            Map.put(state, device, pid) #TODO: this pid cannot be trusted, and is not used
-#          {_} -> nil
-#        end
-#      else
-#        state
-#      end
-#    end
-#    state = appearances |> Enum.reduce(state, adder)
-##    for device <- appearances do
-##      
-###      # supervisor
-###      state = Map.put(state, device, 42)
-##    end
     
     for {device, description} <- uarts do
       IO.puts("- #{device}")
@@ -82,7 +37,6 @@ defmodule Scanner do
           {:error, _} -> nil
         end
       end
-#      state = Map.put_new_lazy(state, v, GenServer.start(Gateway, v))
     end
     
     Process.send_after(self(), :scan, @sleeptime)
