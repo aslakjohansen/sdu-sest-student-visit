3f88976d2d2ea9a7d1d935d19c9eeebb187e3e7f
Aslak Johansen <aslakj@gmail.com>
2021-10-23 20:54:53 +0200

    Wrote slides for string and object

---
+++ src/frontend/part3_object/logic.js
@@ -0,0 +1,19 @@
+data = {"temp": 21.3, "hum": 61.7};
+result = "<p>Indhold:</p>";
+
+keys = Object.keys(data);
+result += "<ul>"
+for (i=0 ; i<keys.length ; i++) {
+  key = keys[i];
+  value = data[key]
+  result += "<li><b>"+key+"</b> peger på <i>værdien</i> "+value+".</li>";
+}
+result += "</ul>"
+
+if ('temp' in data)
+  result += "<p>data inddeholder nøglen 'temp'</p>"
+
+if ('wind' in data)
+  result += "<p>data inddeholder nøglen 'wind'</p>"
+
+document.getElementById("data").innerHTML = result
