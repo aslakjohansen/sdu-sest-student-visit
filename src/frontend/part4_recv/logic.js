4af2943dec18d254a869e01500d9c77c08ee04ef
Aslak Johansen <aslakj@gmail.com>
2021-10-23 23:21:57 +0200

    Added semicolons

--- src/frontend/part4_recv/logic.js
+++ src/frontend/part4_recv/logic.js
@@ -1,5 +1,5 @@
-client_id = "client"+Date.now()
-topic = "dk/sdu/sest/test"
+client_id = "client"+Date.now();
+topic = "dk/sdu/sest/test";
 
 // Create a client instance
 client = new Paho.MQTT.Client("broker.hivemq.com", 8000, client_id);
