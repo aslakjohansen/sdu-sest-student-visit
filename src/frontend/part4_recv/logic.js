a9928a660deb76ab7dcc20c86191efc2fe37de47
Aslak Johansen <aslakj@gmail.com>
2021-10-23 23:19:13 +0200

    Added slides for receiving MQTT data

---
+++ src/frontend/part4_recv/logic.js
@@ -0,0 +1,16 @@
+client_id = "client"+Date.now()
+topic = "dk/sdu/sest/test"
+
+// Create a client instance
+client = new Paho.MQTT.Client("broker.hivemq.com", 8000, client_id);
+client.onMessageArrived = onMessageArrived;
+client.connect({onSuccess:onConnect});
+
+function onConnect() {
+  client.subscribe(topic);
+}
+
+function onMessageArrived(message) {
+  document.getElementById("data").innerHTML = message.payloadString;
+}
+
