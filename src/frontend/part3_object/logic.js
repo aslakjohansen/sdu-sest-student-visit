85ef0437a02e5493c969081ec4a3132a8915a454
Aslak Johansen <aslakj@gmail.com>
2021-10-23 23:24:17 +0200

    Added semicolons

--- src/frontend/part3_object/logic.js
+++ src/frontend/part3_object/logic.js
@@ -2,17 +2,17 @@ data = {"temp": 21.3, "hum": 61.7};
 result = "<p>Indhold:</p>";
 
 keys = Object.keys(data);
-result += "<ul>"
+result += "<ul>";
 for (i=0 ; i<keys.length ; i++) {
   key = keys[i];
-  value = data[key]
+  value = data[key];
   result += "<li><b>"+key+"</b> peger på <i>værdien</i> "+value+".</li>";
 }
-result += "</ul>"
+result += "</ul>";
 
 if ('temp' in data)
-  result += "<p>data inddeholder nøglen 'temp'</p>"
+  result += "<p>data inddeholder nøglen 'temp'</p>";
 if ('wind' in data)
-  result += "<p>data inddeholder nøglen 'wind'</p>"
+  result += "<p>data inddeholder nøglen 'wind'</p>";
 
-document.getElementById("data").innerHTML = result
+document.getElementById("data").innerHTML = result;
